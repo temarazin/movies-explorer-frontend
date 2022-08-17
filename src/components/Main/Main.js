184a7f21d537ca5ba0cafc6d888ea2c1b57220a1
temarazin <temarazin@gmail.com>
2022-08-18 00:59:56 +0300

    feat: add techs section

--- src/components/Main/Main.js
+++ src/components/Main/Main.js
@@ -1,11 +1,13 @@
 import Intro from './Intro/Intro';
 import About from './About/About';
+import Techs from './Techs/Techs';
 
 function Main() {
   return (
     <main className="main">
       <Intro />
       <About />
+      <Techs />
     </main>
   );
 }
