ae803ead14ea86edf3480c96e99b7faea4123324
temarazin <temarazin@gmail.com>
2022-08-25 07:38:46 +0300

    fix: update Button component

--- src/components/common/Button/Button.js
+++ src/components/common/Button/Button.js
@@ -1,19 +1,34 @@
+import { Link } from "react-router-dom";
 import "./Button.css";
 
-function Button({ children, className = "button", link = false, ...props }) {
-
-  if (link) {
-    return (
-      <a href={link} className={className} {...props}>
-        {children}
-      </a>
-    );
-  } else {
-    return (
-      <button className={className} {...props}>
-        {children}
-      </button>
-    );
+function Button({
+  children,
+  className = "button",
+  elemType = "button",
+  link = "/",
+  ...props
+}) {
+  switch (elemType) {
+    case "button":
+      return (
+        <button className={className} {...props}>
+          {children}
+        </button>
+      );
+    case "link":
+      return (
+        <Link to={link} className={className} {...props}>
+          {children}
+        </Link>
+      );
+    case "a":
+      return (
+        <a href={link} className={className} {...props}>
+          {children}
+        </a>
+      );
+    default:
+      return false;
   }
 }
 
