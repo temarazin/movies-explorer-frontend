cc70660de34ff48f496ca5435e52ddd1cf688dfd
temarazin <temarazin@gmail.com>
2022-08-27 05:53:20 +0300

    feat: adapt movie cards to screen width

--- src/components/common/MoviesCardList/MoviesCardList.js
+++ src/components/common/MoviesCardList/MoviesCardList.js
@@ -1,10 +1,39 @@
+import { useState, useEffect } from "react";
+
 import MoviesCard from "../MoviesCard/MoviesCard";
 import Button from "../Button/Button";
 import Preloader from "../Preloader/Preloader";
+import useWindowDimensions from "../../../utils/hooks/useWindowDimensions";
 
 import "./MoviesCardList.css";
 
 function MoviesCardList({ isUserMovies = false, films, isLoading }) {
+  const { width: windowWidth } = useWindowDimensions();
+  const [filmsPerLoad, setFilmsPerLoad] = useState(0);
+  const [filmsCount, setFilmsCount] = useState(0);
+
+  useEffect(() => {
+    if (windowWidth > 960) {
+      setFilmsPerLoad(4);
+    } else if (windowWidth > 768) {
+      setFilmsPerLoad(3);
+    } else if (windowWidth > 480) {
+      setFilmsPerLoad(2);
+    } else {
+      setFilmsPerLoad(5);
+    }
+  }, [windowWidth]);
+
+  useEffect(() => {
+    if (filmsPerLoad !== 0) {
+      setFilmsCount(Math.floor(filmsCount / filmsPerLoad) * filmsPerLoad + filmsPerLoad);
+    }
+    console.log(filmsCount);
+  }, [filmsPerLoad]);
+
+  function handleMoreBtn() {
+    setFilmsCount(filmsCount + filmsPerLoad);
+  }
 
   return (
     <section className="movies-card-list">
@@ -13,19 +42,22 @@ function MoviesCardList({ isUserMovies = false, films, isLoading }) {
           <Preloader />
         ) : (
           <>
+            {films.length === 0 && <p class="movies-card-list__no-results">Ничего не найдено</p>}
             <ul className="movies-card-list__cards">
-              {films.map((item) => (
+              {films.slice(0, filmsCount).map((item) => (
                 <li className="movies-card-list__item" key={item.id}>
-                  <MoviesCard
-                    onlyRemove={isUserMovies}
-                    data={item}
-                  />
+                  <MoviesCard onlyRemove={isUserMovies} data={item} />
                 </li>
               ))}
             </ul>
-            <Button className="button button_theme_more movies-card-list__linkmore">
-              Еще
-            </Button>
+            {filmsCount < films.length && (
+              <Button
+                className="button button_theme_more movies-card-list__linkmore"
+                onClick={handleMoreBtn}
+              >
+                Еще
+              </Button>
+            )}
           </>
         )}
       </div>
